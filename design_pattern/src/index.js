e0223c1b8f405b78145ce91104eddca62f69548f
sakurahuang <sakurahuang@tencent.com>
2024-07-01 16:23:06 +0800

    停车场完结

--- design_pattern/src/index.js
+++ design_pattern/src/index.js
@@ -1,13 +1,19 @@
+/*
+ * @Author: sakurahuang
+ * @Description: 
+ * @Date: 2024-06-26 16:00:16
+ */
 var inherit = require('./3Elements/inherit.js')
 var polymorphic = require('./3Elements/polymorphic.js')
 var jqueryApply = require('./3Elements/jqueryApply.js')
-class Person {
-    constructor(name) {
-        this.name = name
-    }
-    getName() {
-        return this.name
-    }
-}
-let p = new Person('bacra')
-p.getName()
+const parking = require('./3Elements/parking.js')
+// class Person {
+//     constructor(name) {
+//         this.name = name
+//     }
+//     getName() {
+//         return this.name
+//     }
+// }
+// let p = new Person('bacra')
+// p.getName()
